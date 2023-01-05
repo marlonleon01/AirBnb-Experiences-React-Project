6fe05c03ff00152f6da650444a5f6a6390af55d4
Marlon Leon <ghostveene@gmail.com>
2023-01-05 12:41:10 -0500

    setup with navbar and hero

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import { Navbar } from "./Navbar";
+import { Hero } from "./Hero";
+
+export function App() {
+    return (
+        <div>
+            <Navbar />
+            <Hero />
+        </div>
+    )
+}
