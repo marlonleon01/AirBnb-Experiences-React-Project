e54df9be60a903a4291e319c94e07d3d483113fa
Marlon Leon <ghostveene@gmail.com>
2023-01-05 12:57:59 -0500

    imported fontawesome icons and created navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,3 +1,12 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faAirbnb} from "@fortawesome/free-brands-svg-icons"
+
 export function Navbar() {
-    
+    return (
+        <nav>
+            <h1>
+                <FontAwesomeIcon icon={faAirbnb}></FontAwesomeIcon>Airbnb
+            </h1>
+        </nav>
+    )
 }
