10bae9bae43b089cac41cf64061220360222a137
Marlon Leon <ghostveene@gmail.com>
2023-01-05 13:33:32 -0500

    took icon out of h1 element

--- src/Navbar.js
+++ src/Navbar.js
@@ -4,9 +4,8 @@ import {faAirbnb} from "@fortawesome/free-brands-svg-icons"
 export function Navbar() {
     return (
         <nav className="navbar">
-            <h1 className="airbnb-title">
-                <FontAwesomeIcon icon={faAirbnb} className="airbnb-icon"></FontAwesomeIcon>Airbnb
-            </h1>
+            <FontAwesomeIcon icon={faAirbnb} className="airbnb-icon"></FontAwesomeIcon>
+            <h1 className="airbnb-title">Airbnb</h1>
         </nav>
     )
 }
