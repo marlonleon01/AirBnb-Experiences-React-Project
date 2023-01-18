31e8394dad39aa76b2c0ec937f5c7b961f887d3f
Marlon Leon <ghostveene@gmail.com>
2023-01-18 06:34:18 -0500

    moved into component folder

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+import {faAirbnb} from "@fortawesome/free-brands-svg-icons"
+
+export function Navbar() {
+    return (
+        <nav className="navbar">
+            <FontAwesomeIcon icon={faAirbnb} className="airbnb-icon"></FontAwesomeIcon>
+            <h1 className="airbnb-title">airbnb</h1>
+        </nav>
+    )
+}
