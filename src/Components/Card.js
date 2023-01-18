92e29151de8510cd54466eacf88854b82fa8c789
Marlon Leon <ghostveene@gmail.com>
2023-01-18 06:50:55 -0500

    hard coded data will be changing

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,7 +1,16 @@
+import Katie from "../imgs/Katie Zaferes.png"
+import Star from "../imgs/Star.png"
+
 export function Card() {
     return (
         <div>
-            
+            <img src={Katie} className="katie"></img>
+            <div>
+                <img src={Star} className="star"></img>
+                <span className="card-rating">5.0 (6)•USA</span>
+                <h2 className="card-title">Life lessons with Katie Zaferes</h2>
+                <p className="card-price"><strong>From $136</strong> / person</p>
+            </div>
         </div>
     )
 }
