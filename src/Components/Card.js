6ba300c8bf278b8b31ab989b406ea71f43c4d7ac
Marlon Leon <ghostveene@gmail.com>
2023-01-18 07:17:21 -0500

    added all elements change strong tag to span

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,14 +3,16 @@ import Star from "../imgs/Star.png"
 
 export function Card() {
     return (
-        <div>
-            <img src={Katie} className="katie"></img>
-            <div>
-                <img src={Star} className="star"></img>
-                <span className="card-rating">5.0 (6)•USA</span>
-                <h2 className="card-title">Life lessons with Katie Zaferes</h2>
-                <p className="card-price"><strong>From $136</strong> / person</p>
+        <div className="card">
+            <img src={Katie} className="card-img"></img>
+            <div className="card-info">
+                <img src={Star} className="card-star"></img>
+                <span className="card-rating">5.0</span>
+                <span className="gray">(6) • </span>
+                <span className="gray">USA</span>
             </div>
+                <p className="card-title">Life lessons with Katie Zaferes</p>
+                <p className="card-price"><strong>From $136</strong> / person</p>
         </div>
     )
 }
