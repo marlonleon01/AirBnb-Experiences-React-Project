68b85ce33dc3ad321969cb0636da319fcb2db618
Marlon Leon <ghostveene@gmail.com>
2023-01-18 06:37:56 -0500

    added card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Navbar } from "./Components/Navbar";
 import { Hero } from "./Components/Hero";
+import { Card } from "./Components/Card"
 
 export function App() {
     return (
         <div className="all-content">
             <Navbar />
             <Hero />
+            <Card />
         </div>
     )
 }
