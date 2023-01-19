942b79f9da87737acf492f1ba8b6c731bfddefaf
Marlon Leon <ghostveene@gmail.com>
2023-01-19 11:52:59 -0500

    fixed display of badge

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,7 +1,14 @@
 export function Card(props) {
+    let badgeText 
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+    
     return (
         <div className="card">
-            {props.openSpots === 0 && <div className="card-badge">SOLD OUT</div>}
+            {badgeText && <div className="card-badge">{badgeText}</div>}
             <img src={props.img} className="card-img"></img>
             <div className="card-info">
                 <img src="imgs/Star.png" className="card-star"></img>
