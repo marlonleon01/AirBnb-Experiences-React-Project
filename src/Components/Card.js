f2f3543ae323b4758cfd291e772a00b25803abdd
Marlon Leon <ghostveene@gmail.com>
2023-01-19 10:19:57 -0500

    changed country to location

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -8,7 +8,7 @@ export function Card(props) {
                 <img src={Star} className="card-star"></img>
                 <span className="card-rating">{props.rating}</span>
                 <span className="gray">({props.reviewCount}) • </span>
-                <span className="gray">{props.country}</span>
+                <span className="gray">{props.location}</span>
             </div>
                 <p className="card-title">{props.title}</p>
                 <p className="card-price"><span className="bold">{props.price}</span> / person</p>
