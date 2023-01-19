639fd4167191ef6df6b50ca70ba679272813bd4b
Marlon Leon <ghostveene@gmail.com>
2023-01-19 10:39:40 -0500

    moved imgs to public folder

--- src/data.js
+++ src/data.js
@@ -1,14 +1,10 @@
-import Katie from "./imgs/Katie Zaferes.png"
-import WeddingPhoto from "./imgs/Wedding Photo.png"
-import MountainBike from "./imgs/Mountain Bike.png"
-
 export default [
     {
         id: 1,
         title: "Life Lessons with Katie Zaferes",
         description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
         price: 136,
-        coverImg: {Katie},
+        coverImg: "imgs/Katie Zaferes.png",
         stats: {
             rating: 5.0,
             reviewCount: 6
@@ -21,7 +17,7 @@ export default [
         title: "Learn Wedding Photography",
         description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
         price: 125,
-        coverImg: {WeddingPhoto},
+        coverImg: "imgs/Wedding Photo.png",
         stats: {
             rating: 5.0,
             reviewCount: 30
@@ -34,7 +30,7 @@ export default [
         title: "Group Mountain Biking",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 50,
-        coverImg: {MountainBike},
+        coverImg: "imgs/Mountain Bike.png",
         stats: {
             rating: 4.8,
             reviewCount: 2
