9ce3d264ca6c2f300b064d1bfea11a2c39407d2b
Marlon Leon <ghostveene@gmail.com>
2023-01-19 09:44:44 -0500

    using props it will be changed to make it more reusable

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { Navbar } from "./Components/Navbar";
 import { Hero } from "./Components/Hero";
 import { Card } from "./Components/Card"
+import Katie from "./imgs/Katie Zaferes.png"
 
 export function App() {
     return (
         <div className="all-content">
             <Navbar />
             <Hero />
-            <Card />
+            <Card 
+                img={Katie}
+                rating="5.0"
+                reviewCount={6}
+                country="USA"
+                title="Life Lessons with Katie Zaferes"
+                price={136}
+            />
         </div>
     )
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,18 +1,17 @@
-import Katie from "../imgs/Katie Zaferes.png"
 import Star from "../imgs/Star.png"
 
-export function Card() {
+export function Card(props) {
     return (
         <div className="card">
-            <img src={Katie} className="card-img"></img>
+            <img src={props.img} className="card-img"></img>
             <div className="card-info">
                 <img src={Star} className="card-star"></img>
-                <span className="card-rating">5.0</span>
-                <span className="gray">(6) • </span>
-                <span className="gray">USA</span>
+                <span className="card-rating">{props.rating}</span>
+                <span className="gray">({props.reviewCount}) • </span>
+                <span className="gray">{props.country}</span>
             </div>
-                <p className="card-title">Life lessons with Katie Zaferes</p>
-                <p className="card-price"><span className="bold">From $136</span> / person</p>
+                <p className="card-title">{props.title}</p>
+                <p className="card-price"><span className="bold">{props.price}</span> / person</p>
         </div>
     )
 }
